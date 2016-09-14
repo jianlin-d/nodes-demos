604386dda6af1d5227dd73a7c5d649a83b8a6a9c
Jianlin <dongjianlin@gmail.com>
2016-09-14 16:57:08 +0800

    add os module demo

---
+++ demo02-os/os.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Jason on 14/09/2016.
+ */
+const os = require('os');
+
+console.log('\nhostname:\n-----------------');
+console.log(os.hostname());
+console.log('\n');
+
+console.log('os type:\n---------------------');
+console.log(os.type());
+console.log('\n');
+
+console.log('platform :\n---------------------');
+console.log(`${os.platform()} ${os.release()}` );
+console.log('\n');
+
+
+
+console.log('cpu information:\n---------------------');
+os.cpus().forEach(function (cpu) {
+    console.log(cpu);
+});
+console.log('\n');
+
+console.log('network interfaces:\n---------------------');
+console.log(os.networkInterfaces());
+console.log('\n');
+
+
+
+//not correct in macOS
+//console.log(`\nyour current free memory:\n-----------------\n${(os.freemem()/Math.pow(1024,2)).toFixed(2)} MB`);
